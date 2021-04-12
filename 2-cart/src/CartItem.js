d8e11be049d0f72b2a2f9cdbdcb9a9294985fa3e
Dhruv <dhruv@rsocial.in>
2021-04-12 16:12:07 +0530

    Added state and icons to cart item

--- 2-cart/src/CartItem.js
+++ 2-cart/src/CartItem.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
 
 class CartItem extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            price: 999,
+            title: 'M Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+
     render() {
+        // Object destructuring
+        const {price, title, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}></img>
                 </div>
                 <div className="right-block">
-                <div style={{fontSize:25}}>Phone</div>
-                <div style={{color:'#777'}}>Rs 999</div>
-                <div style={{color:'#777'}}>Qty: 1</div>
+                <div style={{fontSize:25}}>{this.state.title}</div>
+                <div style={{color:'#777'}}>Rs {price}</div>
+                <div style={{color:'#777'}}>Qty: {qty}</div>
                 <div className="cart-item-actions">
-                
+                    <img alt="increase" className="action-icons" src="https://www.flaticon.com/premium-icon/icons/svg/3303/3303893.svg"/>
+                    <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1618224573~hmac=1278343fdd553b92019ef78f4b814b1f"/>
+                    <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1345/1345823.svg?token=exp=1618224673~hmac=d264c11092b363f49300b401ba462ffa"/>
                 </div>
                 
                 </div>
