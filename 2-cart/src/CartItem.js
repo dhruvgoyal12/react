fd3f2cb108b7fd858fe7a7e4e15f4f4df1248148
Dhruv <dhruv@rsocial.in>
2021-04-12 16:51:33 +0530

    Added increase decrease functionality in cart item

--- 2-cart/src/CartItem.js
+++ 2-cart/src/CartItem.js
@@ -12,6 +12,31 @@ class CartItem extends Component {
         }
     }
 
+    increaseQuantity(){
+        // setState form 1
+        // this.setState({
+        //     qty : this.state.qty + 1
+        // });
+
+        // setState form 2
+        this.setState((prevState) => {
+            return {
+            qty: prevState.qty + 1
+            }
+        });
+    }
+
+    // Using arrow operator we don't require binding
+    decreaseQuantity = () => {
+        this.setState((prevState) => {
+            if (prevState.qty > 0)
+            return {
+            qty: prevState.qty -1
+            }
+
+        });
+    }
+
 
     render() {
         // Object destructuring
@@ -26,8 +51,8 @@ class CartItem extends Component {
                 <div style={{color:'#777'}}>Rs {price}</div>
                 <div style={{color:'#777'}}>Qty: {qty}</div>
                 <div className="cart-item-actions">
-                    <img alt="increase" className="action-icons" src="https://www.flaticon.com/premium-icon/icons/svg/3303/3303893.svg"/>
-                    <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1618224573~hmac=1278343fdd553b92019ef78f4b814b1f"/>
+                    <img alt="increase" className="action-icons" src="https://www.flaticon.com/premium-icon/icons/svg/3303/3303893.svg" onClick={this.increaseQuantity.bind(this)}/>
+                    <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1618224573~hmac=1278343fdd553b92019ef78f4b814b1f" onClick={this.decreaseQuantity}/>
                     <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1345/1345823.svg?token=exp=1618224673~hmac=d264c11092b363f49300b401ba462ffa"/>
                 </div>
                 
