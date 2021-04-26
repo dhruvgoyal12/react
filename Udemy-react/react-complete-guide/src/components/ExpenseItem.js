2d6b1fe6230d5f471aac738cd9deeaed09d462cc
Dhruv <dhruv@rsocial.in>
2021-04-26 21:10:23 +0530

    Custom expense item with css and props

---
+++ Udemy-react/react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const month = props.expense.date.toLocaleString("en-US", { month: "long" });
+  const day = props.expense.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.expense.date.getFullYear();
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.expense.title}</h2>
+        <div className="expense-item__price">{props.expense.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
