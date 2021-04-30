6da6305375329a108e41fce3cdd4def5faaf4e8e
Dhruv <dhruv@rsocial.in>
2021-04-30 20:36:05 +0530

    Added year dropdown

--- Udemy-react/react-complete-guide/src/components/Expenses/Expenses.js
+++ Udemy-react/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 
 function Expenses(props) {
+  const [filterYear, setFilterYear] = useState("2020");
+  const pickedYearHandler = (year) => {
+    setFilterYear(year);
+  };
+
   return props.expenses.map((expense) => {
     return (
       <Card className="expenses">
+        <ExpensesFilter
+          dropdownStartYear={filterYear}
+          onYearFilter={pickedYearHandler}
+        />
         <ExpenseItem expense={expense} key={expense.id} />
       </Card>
     );
