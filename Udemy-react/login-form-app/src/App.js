ec79b2573f4081d2861931bf88f66ab6c2c4ee13
Dhruv <dhruv@rsocial.in>
2021-05-16 21:29:42 +0530

    Using useEffect for form validity

--- Udemy-react/login-form-app/src/App.js
+++ Udemy-react/login-form-app/src/App.js
@@ -22,7 +22,7 @@ function App() {
   };
 
   const logoutHandler = () => {
-    localStorage.setItem("isLoggedIn", "0");
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
