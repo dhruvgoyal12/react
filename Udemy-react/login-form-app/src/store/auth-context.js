88e3953215f7bf8b90baeb03381b7966ed894ea0
Dhruv <dhruv@rsocial.in>
2021-05-17 19:21:02 +0530

    Using react store to check isLogged In

---
+++ Udemy-react/login-form-app/src/store/auth-context.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
