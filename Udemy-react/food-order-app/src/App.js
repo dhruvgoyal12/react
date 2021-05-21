a4c129ee6b6a78acc08a7fd3d6940a1314df73c3
Dhruv <dhruv@rsocial.in>
2021-05-21 17:08:25 +0530

    Added meals summary and basic list

--- Udemy-react/food-order-app/src/App.js
+++ Udemy-react/food-order-app/src/App.js
@@ -1,10 +1,13 @@
+import { Fragment } from "react";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <Header />
-    </div>
+      <Meals />
+    </Fragment>
   );
 }
 
