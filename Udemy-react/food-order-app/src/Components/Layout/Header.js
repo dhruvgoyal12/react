aef5dbff6e9e286bcdc30f3dcd9ac6a16e60860d
Dhruv <dhruv@rsocial.in>
2021-05-21 17:07:16 +0530

    Added Cart Button

--- Udemy-react/food-order-app/src/Components/Layout/Header.js
+++ Udemy-react/food-order-app/src/Components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 export default function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of food" />
--- Udemy-react/food-order-app/src/Components/Layout/HeaderCartButton.js
+++ Udemy-react/food-order-app/src/Components/Layout/HeaderCartButton.js
@@ -1,5 +1,15 @@
 import React from "react";
+import CartIcon from "./CartIcon";
+import classes from "./HeaderCartButton.module.css";
 
 export default function HeaderCartButton(props) {
-  return <button></button>;
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
 }
