4787ca36afe155b969d2f1f4932293740e9a7d88
Dhruv <dhruv@rsocial.in>
2021-05-23 14:27:02 +0530

    Basic project

---
+++ Udemy-react/class-based-components/src/components/User.js
@@ -0,0 +1,18 @@
+import classes from './User.module.css';
+import React, { Component } from 'react'
+
+export default class User extends Component {
+  render() {
+    return (
+      <li className={classes.user}>{this.props.name}</li>
+    )
+  }
+}
+
+
+
+// const User = (props) => {
+//   return 
+// };
+
+// export default User;
