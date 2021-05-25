9403a61d85ea5a347c6da75e281e56ea517839a3
Dhruv <dhruv@rsocial.in>
2021-05-25 15:37:21 +0530

    Added custom useHttp hook

--- Udemy-react/custom-hooks-example2/src/App.js
+++ Udemy-react/custom-hooks-example2/src/App.js
@@ -2,38 +2,57 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://react-http-bc473-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks
+  );
+
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-bc473-default-rtdb.firebaseio.com/tasks.json"
-      );
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://react-http-bc473-default-rtdb.firebaseio.com/tasks.json"
+  //     );
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
 
-      const data = await response.json();
+  //     const data = await response.json();
 
-      const loadedTasks = [];
+  //     const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
 
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
 
   useEffect(() => {
     fetchTasks();
---
+++ Udemy-react/custom-hooks-example2/src/hooks/use-http.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+// "https://react-http-bc473-default-rtdb.firebaseio.com/tasks.json"
+
+export default function useHttp(requestConfig, applyFunction) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyFunction(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return { isLoading: isLoading, error: error, sendRequest: sendRequest };
+}
