e033bf24bd765a6bc652132cdf830679a0e7e7f4
Dhruv <dhruv@rsocial.in>
2021-05-25 22:25:59 +0530

    Added validation and some error text on submit and on lose focus

--- Udemy-react/form-user-input/src/components/SimpleInput.js
+++ Udemy-react/form-user-input/src/components/SimpleInput.js
@@ -2,25 +2,55 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-  const formSubmitHandler = (event) => {
-    event.preventDefaults();
-    const enteredValue = nameInputRef.current.value;
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+    if (enteredName.trim() == "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
   };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() == "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+    const enteredValue = nameInputRef.current.value;
+
+    //nameInputRef.current.value = ''; => NOT IDEAL, DO NOT MANIPULATE DOM ON YOUR OWN
+    setEnteredName("");
+  };
+
+  const nameInputClasses =
+    !enteredNameIsValid && enteredNameTouched
+      ? "form-control invalid"
+      : "form-control";
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          value={enteredName}
+          onBlur={nameInputBlurHandler}
         />
+        {!enteredNameIsValid && enteredNameTouched && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
