1ad091c3cdf44335eed1f6bebea70adb39454a02
Dhruv <dhruv@rsocial.in>
2021-05-29 21:48:59 +0530

    Added multiple slices and auth reducer

--- Udemy-react/redux-project/src/store/index.js
+++ Udemy-react/redux-project/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
+const initialAuthState = { isAuth: false };
+
 const counterSlice = createSlice({
   name: "counter",
-  initialState: initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -21,7 +23,21 @@ const counterSlice = createSlice({
   },
 });
 
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuth = true;
+    },
+    logout(state) {
+      state.isAuth = false;
+    },
+  },
+});
+
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 // const storeReducer = (state = initialState, action) => {
 //   if (action.type === "increment") {
@@ -47,7 +63,7 @@ export const counterActions = counterSlice.actions;
 // const store = createStore(storeReducer);
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export default store;
