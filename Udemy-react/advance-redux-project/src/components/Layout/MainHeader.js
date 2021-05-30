431f2e7c8e44d2078a4a17fda9ff56a07a3f025e
Dhruv <dhruv@rsocial.in>
2021-05-30 14:56:20 +0530

    Refresher project

--- Udemy-react/advance-redux-project/src/components/Layout/MainHeader.js
+++ Udemy-react/advance-redux-project/src/components/Layout/MainHeader.js
@@ -1,14 +1,23 @@
-import CartButton from '../Cart/CartButton';
-import classes from './MainHeader.module.css';
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+import CartButton from "../Cart/CartButton";
+import classes from "./MainHeader.module.css";
 
 const MainHeader = (props) => {
+  const dispatch = useDispatch();
+
+  const cartHandler = () => {
+    console.log("clicked");
+    dispatch(uiActions.toggle());
+  };
+
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
       <nav>
         <ul>
           <li>
-            <CartButton />
+            <CartButton onClick={cartHandler} />
           </li>
         </ul>
       </nav>
