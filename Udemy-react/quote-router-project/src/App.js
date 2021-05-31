4d94423115f8bc7bc17ba661405d15fdd8a1251e
Dhruv <dhruv@rsocial.in>
2021-05-31 20:41:23 +0530

    Added basic routes and Components

--- Udemy-react/quote-router-project/src/App.js
+++ Udemy-react/quote-router-project/src/App.js
@@ -1,7 +1,22 @@
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId" exact>
+          <QuoteDetail />
+        </Route>
+        <Route path="/quotes/add">
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
 }
