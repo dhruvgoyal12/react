49680879ba8d3be655a8667cb33f955c5253b9e8
Dhruv <dhruv@rsocial.in>
2021-06-01 16:18:57 +0530

    Added quoteForm prompt

--- Udemy-react/quote-router-project/src/pages/NewQuote.js
+++ Udemy-react/quote-router-project/src/pages/NewQuote.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useHistory } from "react-router";
 import QuoteForm from "./../components/quotes/QuoteForm";
 
-const addQuoteHandler = (quoteData) => {};
-
 export default function NewQuote() {
+  const history = useHistory();
+
+  const addQuoteHandler = (quoteData) => {
+    history.push("/quotes");
+  };
   return <QuoteForm onAddQuote={addQuoteHandler}></QuoteForm>;
 }
