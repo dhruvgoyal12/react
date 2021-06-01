2e619a299c92ceebbe7a05d357090d46dc4eb592
Dhruv <dhruv@rsocial.in>
2021-06-01 16:07:32 +0530

    Added not found page

--- Udemy-react/quote-router-project/src/pages/QuoteDetail.js
+++ Udemy-react/quote-router-project/src/pages/QuoteDetail.js
@@ -1,12 +1,23 @@
 import React, { Fragment } from "react";
 import { Route, useParams } from "react-router";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote.js";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning react is fun" },
+  { id: "q2", author: "Maxi", text: "Imaginations" },
+];
 
 export default function QuoteDetail() {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p> No quote found</p>;
+  }
   return (
     <Fragment>
-      {params.quoteId}
+      <HighlightedQuote text={quote.text} author={quote.author} />
 
       <Route path={`/quotes/:quoteId/comments`}>
         <Comments />
